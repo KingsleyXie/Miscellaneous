5085e6886f83fa0c9113199d9190d9a229e2cafa
Kingsley <kingsley.xym@gmail.com>
2017-09-26 10:53:06 +0800

    Beta Code

---
+++ Array2D.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Array2D {
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("Input Whatever");
+		JOptionPane.showMessageDialog(null, input);
+		System.exit(0);
+	}
+}
