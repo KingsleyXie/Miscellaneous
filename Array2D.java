5ba30dffc9cbc356a2f359c2b464a08671ea932e
Kingsley <kingsley.xym@gmail.com>
2017-09-26 11:13:47 +0800

    Beta Layout

--- Array2D.java
+++ Array2D.java
@@ -1,9 +1,27 @@
+import java.awt.BorderLayout;
 import javax.swing.*;
 
-public class Array2D {
+public class Array2D extends JFrame {
+	private JButton north = new JButton("N");
+	private JButton south = new JButton("S");
+	private JButton east = new JButton("E");
+	private JButton west = new JButton("W");
+	private JButton center = new JButton("C");
+
+	public Array2D() {
+		setSize(300, 650);
+		setLocation(300, 50);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+
+		add(north, BorderLayout.NORTH);
+		add(south, BorderLayout.SOUTH);
+		add(east, BorderLayout.EAST);
+		add(west, BorderLayout.WEST);
+		add(center, BorderLayout.CENTER);
+	}
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog("Input Whatever");
-		JOptionPane.showMessageDialog(null, input);
-		System.exit(0);
+		Array2D frame = new Array2D();
+		frame.setVisible(true);
 	}
 }
