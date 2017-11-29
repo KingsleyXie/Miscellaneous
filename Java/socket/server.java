07b2cd90a1340238bc92e8d22f53815d6b23a565
Kingsley <kingsley.xym@gmail.com>
2017-11-29 14:32:07 +0800

    Initial Multi-Thread Server Socket

--- Java/socket/server.java
+++ Java/socket/server.java
@@ -1,35 +1,56 @@
 import java.io.*;
 import java.net.*;
 
+class multiServer extends Thread {
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+
+	public static final String closeFlag = "Bye";
+
+	public multiServer(Socket s) throws IOException {
+		socket = s;
+		in = new BufferedReader(
+			new InputStreamReader(socket.getInputStream())
+		);
+
+		out = new PrintWriter(new BufferedWriter(
+			new OutputStreamWriter(socket.getOutputStream())
+		), true);
+	}
+
+	public void run() {
+		System.out.println("Established connection with client\n");
+		try {
+			while (true) {
+					String str = in.readLine();
+					if (str.equals(closeFlag)) break;
+
+					System.out.println("\tReceived: " + str + "\n");
+					out.println("I have received your message: " + str);
+			}
+			System.out.println("Terminated connection with client\n");
+			socket.close();
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+}
+
 public class server {
 	public static final int PORT = 2333;
-	public static final String closeFlag = "Bye";
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket s = new ServerSocket(PORT);
 		System.out.println("Socket server started at port " + PORT);
 
-		Socket socket = s.accept();
-		System.out.println("Established connection with client\n");
-
-		BufferedReader in = new BufferedReader(
-			new InputStreamReader(socket.getInputStream())
-		);
-
-		PrintWriter out = new PrintWriter(new BufferedWriter(
-			new OutputStreamWriter(socket.getOutputStream())
-		), true);
-
 		while (true) {
-			String str = in.readLine();
-			if (str.equals(closeFlag)) break;
-
-			System.out.println("\tReceived: " + str + "\n");
-			out.println("I have received your message: " + str);
+			Socket socket = s.accept();
+			try {
+				new multiServer(socket).start();
+			} catch (Exception e) {
+				e.printStackTrace(); break;
+			}
 		}
-
-		socket.close();
-		System.out.println("Socket Closed");
 		s.close();
+		System.out.println("Socket server closed");
 	}
 }
