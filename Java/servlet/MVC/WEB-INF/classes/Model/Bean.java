b7625f3398910aa764916f34ee38beeebebe1afc
Kingsley <kingsley.xym@gmail.com>
2017-12-09 18:07:34 +0800

    Write Initial All Messages' Selector

--- Java/servlet/MVC/WEB-INF/classes/Model/Bean.java
+++ Java/servlet/MVC/WEB-INF/classes/Model/Bean.java
@@ -1,7 +1,24 @@
 package Model;
 import java.sql.*;
+import java.util.ArrayList;
+
+class Msg {
+	public int id;
+	public Date postTime;
+	public String nickname, message;
+
+	public Msg(
+		int id, Date postTime,
+		String nickname, String message
+	) {
+		this.id = id; this.postTime = postTime;
+		this.nickname = nickname; this.message = message;
+	}
+}
 
 public class Bean {
+	public ArrayList<Msg> messages;
+
 	public String insert(
 		String database, String username, String password,
 		String nickname, String message
@@ -20,6 +37,35 @@ public class Bean {
 			);
 			s.setString(1, nickname); s.setString(2, message);
 			s.executeUpdate();
+
+			s.close(); con.close();
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+		return "OK";
+	}
+
+	public String getAllMsg(
+		String database, String username, String password
+	) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String conURL =
+				"jdbc:mysql://localhost:3306/" + database +
+				"?useUnicode=true&characterEncoding=UTF-8";
+			Connection con = DriverManager.getConnection(conURL, username, password);
+
+			Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery("SELECT * FROM forum");
+			while (rs.next()) {
+				Msg m = new Msg(
+					rs.getInt("ID"),
+					rs.getDate("postTime"),
+					rs.getString("nickname"),
+					rs.getString("message")
+				);
+				messages.add(m);
+			}
 
 			s.close(); con.close();
 		} catch (Exception e) {
