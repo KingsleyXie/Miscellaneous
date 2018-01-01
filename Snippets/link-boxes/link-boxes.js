8045c788a187914c9f9e01b53911a9afb66b7403
Kingsley <kingsley.xym@gmail.com>
2018-01-02 01:04:27 +0800

    :arrow_up: Make Image ReStyle JavaScript Code Responsive

--- Snippets/link-boxes/link-boxes.js
+++ Snippets/link-boxes/link-boxes.js
@@ -1,4 +1,18 @@
 window.onload = function() {
+	reCenterImgs();
+
+	var mql = window.matchMedia("(max-width: 520px)");
+	var matchState = mql.matches;
+
+	mql.addListener(function(e) {
+		if (e.matches != matchState) {
+			reCenterImgs();
+			matchState = e.matches;
+		}
+	});
+}
+
+function reCenterImgs() {
 	var imgSize = window.innerWidth < 520 ?
 		(window.innerWidth * 0.5 - 22.4) : 150;
 	var imgs = document.getElementsByClassName("avatar");
@@ -6,6 +20,7 @@ window.onload = function() {
 	for(var i = 0; i < imgs.length; i++)
 	{
 		imgs[i].style.maxHeight = imgSize + 'px';
+		imgs[i].style.margin = '0 0';
 
 		var rect = imgs[i].getBoundingClientRect();
 		if (rect.width.toFixed(1) < imgSize) {
