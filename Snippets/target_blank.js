0748de02cfcbbe77c7b4d76b4446ac8f8a0b4c8d
Kingsley <kingsley.xym@gmail.com>
2018-01-03 17:41:51 +0800

    :sparkles: Add Implementation Without JQuery For Target-Blank Snippet

--- Snippets/target_blank.js
+++ Snippets/target_blank.js
@@ -1,15 +1,33 @@
 /***************************************************************************
-  * A Javascript code to set "target=_blank" for `<a>` tags using JQuery
+  * A Javascript code to set "target=_blank" for `<a>` tags
   * Links with different hostname will be opened in a new tab then
   * Inspired By: https://i-meto.com/js-outlink/
   *************************************************************************/
 
+// Implementation With JQuery:
 $(document).ready(function($) {
-	$("a[href*='http://']:not([href^='" +
-		"http://" + location.hostname + "'])")
-	.attr("target", "_blank");
-
 	$("a[href*='https://']:not([href^='" +
 		"https://" + location.hostname+ "'])")
 	.attr("target","_blank");
+
+	$("a[href*='http://']:not([href^='" +
+		"http://" + location.hostname + "'])")
+	.attr("target", "_blank");
 });
+
+// Implementation Without JQuery:
+window.onload = function() {
+	document.querySelectorAll(
+		"a[href*='https://']:not([href^='" +
+		"https://" + location.hostname + "'])"
+	).forEach(function(ele) {
+		ele.target = "_blank";
+	});
+
+	document.querySelectorAll(
+		"a[href*='http://']:not([href^='" +
+		"http://" + location.hostname + "'])"
+	).forEach(function(ele) {
+		ele.target = "_blank";
+	});
+}
