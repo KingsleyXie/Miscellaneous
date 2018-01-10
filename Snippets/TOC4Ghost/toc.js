6507a9e49679e91102b0e4735d46c4811e964008
Kingsley <kingsley.xym@gmail.com>
2018-01-11 01:06:58 +0800

    :tada: Finish Initial Version Of TOC Generater

--- Snippets/TOC4Ghost/toc.js
+++ Snippets/TOC4Ghost/toc.js
@@ -1,24 +1,34 @@
-var TOC = '<div class="toc">文章目录';
-var currHeading = '';
+$(".toc").remove();
+var TOC = '<div class="toc">文章目录<ul>';
 
 var elements = $(".post-content").find(":header");
+var currHeading = elements[0].nodeName;
 
 if (elements.length > 0) {
 	$.each(elements, function(key, content) {
-		if (content.nodeName != currHeading) {
-			TOC += '</ul>' + '<ul>';
-			currHeading = content.nodeName;
-		}
 		var text = content.innerText;
-
+		var link = '<a href="#' + text + '">' + text  + '</a>';
 		content.id = text;
-		TOC +=
-		'<a href="#' + text + '">' +
-			'<li>' + text + '</li>' +
-		'</a>';
+
+		switch (currHeading.localeCompare(content.nodeName)) {
+			case 0:
+				TOC += '</li><li>' + link;
+				break;
+
+			case 1:
+				TOC += '</li></ul><li>' + link;
+				currHeading = content.nodeName;
+				break;
+
+			case -1:
+				TOC += '<ul><li>' + link;
+				currHeading = content.nodeName;
+				break;
+		}
+		console.log(TOC);
 	});
 }
 
-TOC += '</div>';
+TOC += '</ul></div>';
 
 $(".post-content>p:first").before(TOC);
