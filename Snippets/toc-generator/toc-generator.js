1df9fa874ee319ded6d6fa92f0d6f4daf513e639
Kingsley <kingsley.xym@gmail.com>
2018-01-12 21:14:09 +0800

    :bug: Add Content Wrapper Selector Result Checker

--- Snippets/toc-generator/toc-generator.js
+++ Snippets/toc-generator/toc-generator.js
@@ -3,53 +3,64 @@ const config = {
 	"contentWrapper": ".post-content"
 };
 
-var elements =
-	$(config.contentWrapper).find(":header")
-	.filter(":not(blockquote :header)");
 
-if (elements.length > 0) {
-	var TOC = '<div class="toc">' + config.title + '<ul>';
 
-	var currHeading = elements[0].nodeName;
-	var records = new Array();
+var wrapper = $(config.contentWrapper);
+if (wrapper.length > 0) {
+	var elements = wrapper.find(":header")
+		.filter(":not(blockquote :header)");
 
-	$.each(elements, function(key, content) {
-		var text = content.innerText;
-		var link = '<a href="#' + text + '">' + text  + '</a>';
-		content.id = text;
+	if (elements.length > 0) {
+		var TOC = '<div class="toc">' + config.title + '<ul>';
 
-		switch (currHeading.localeCompare(content.nodeName)) {
-			case 0:
-				TOC += '</li><li>' + link;
-				break;
+		var currHeading = elements[0].nodeName;
+		var records = new Array();
 
-			case 1:
-				currHeading = content.nodeName;
+		$.each(elements, function(key, content) {
+			var text = content.innerText;
+			var link = '<a href="#' + text + '">' + text  + '</a>';
+			content.id = text;
 
-				if (!records.includes(currHeading)) {
-					console.warn(
-						'Warning: There may be some problem with your heading structure, ' +
-						'so the generated TOC is not guaranteed to be in right order.'
-					);
-				}
+			switch (currHeading.localeCompare(content.nodeName)) {
+				case 0:
+					TOC += '</li><li>' + link;
+					break;
 
-				while (records.includes(currHeading)) {
-					TOC += '</li></ul>';
-					records.pop();
-				}
-				TOC += '<li>' + link;
-				break;
+				case 1:
+					currHeading = content.nodeName;
 
-			case -1:
-				TOC += '<ul><li>' + link;
-				records.push(currHeading);
-				currHeading = content.nodeName;
-				break;
-		}
-	});
+					if (!records.includes(currHeading)) {
+						console.warn(
+							'Warning: There may be some problem ' +
+							'with your heading structure, ' +
+							'so the generated TOC is not guaranteed ' +
+							'to be in right order.'
+						);
+					}
 
-	TOC += '</ul></div>';
-	$(config.contentWrapper).find(">:first-child").before(TOC);
+					while (records.includes(currHeading)) {
+						TOC += '</li></ul>';
+						records.pop();
+					}
+					TOC += '<li>' + link;
+					break;
+
+				case -1:
+					TOC += '<ul><li>' + link;
+					records.push(currHeading);
+					currHeading = content.nodeName;
+					break;
+			}
+		});
+
+		TOC += '</ul></div>';
+		$(config.contentWrapper).find(">:first-child").before(TOC);
+	} else {
+		console.warn('No heading found to generate TOC.');
+	}
 } else {
-	console.warn('No heading found to generate TOC.');
+	console.warn(
+		'Warning: The provided Selector `' +
+		config.contentWrapper + '` is not valid.'
+	);
 }
