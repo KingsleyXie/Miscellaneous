d72a293c011351b5bcd7b4170a51a68a6ecddd27
Kingsley <kingsley.xym@gmail.com>
2018-03-24 23:12:49 +0800

    :sparkles: Add Initial Battleship Code With Python3

---
+++ Python/Battleship.py
@@ -0,0 +1,18 @@
+from random import randint
+
+board = []
+
+for x in range(5):
+	board.append(['O'] * 5)
+
+def print_board():
+	for row in board:
+		print(' '.join(row))
+
+print("\nLet's play Battleship!\n")
+print_board()
+
+ship_row = randint(0, len(board) - 1)
+ship_col = randint(0, len(board[0]) - 1)
+
+print("\n/// Answer:  (%d, %d) ///" % (ship_row, ship_col))
