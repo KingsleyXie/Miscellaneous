766dfabda75b1987df531366e4c3eec6e042b863
Kingsley <kingsley.xym@gmail.com>
2018-03-25 12:00:06 +0800

    :arrow_up: Integrate More Configurations

--- Python/Battleship.py
+++ Python/Battleship.py
@@ -12,9 +12,18 @@ info = {
 }
 
 # Configuration
-conf = {'rows': 7, 'cols': 7, 'rounds': 5}
+conf = {
+	'rows': 7, # Board row number
+	'cols': 7, # Board column number
+	'rounds': 5, # Round number
 
-# Simplified Customize Option
+	'init': 'O', # Board initial code
+	'guessed': 'X', # Board guessed code
+	'sep': '  ', # Board code separator
+	'ans': True, # Display answer
+}
+
+# Simplified customize option
 if re.match(r'n|N', input(info['customize'])):
 	conf['rows'] = int(input('Set Rows: '))
 	conf['cols'] = int(input('Set Cols: '))
@@ -24,7 +33,7 @@ if re.match(r'n|N', input(info['customize'])):
 # Initialize the board
 board = []
 for x in range(conf['rows']):
-	board.append(['O'] * conf['cols'])
+	board.append([conf['init']] * conf['cols'])
 
 # Generate random ship position
 ship_row = randint(0, len(board) - 1)
@@ -34,18 +43,21 @@ ship_col = randint(0, len(board[0]) - 1)
 def print_board():
 	print()
 	for row in board:
-		print('  '.join(row))
+		print(conf['sep'].join(row))
 
-# Welcome Texts
+# Welcome texts
 print(info['welcome'])
 print_board()
 
-print(
-	' Answer:  ({}, {}) '.format(ship_row, ship_col)
-	.center(3 * conf['cols'] - 2, '*')
-)
+# Display answer
+if conf['ans']:
+	# Center the answer
+	ans_str = ' Answer:  ({}, {}) '.format(ship_row, ship_col)
+	sep_len = len(conf['sep'])
+	ans_width = (sep_len + 1) * conf['cols'] - sep_len
+	print(ans_str.center(ans_width, '*'))
 
-# Game Start
+# Game start
 sep = '-' * 13 + ' ' * 3
 for turn in range(conf['rounds']):
 	print('\n\n{}Round {}/{}{}\n'.format(
@@ -63,10 +75,10 @@ for turn in range(conf['rounds']):
 		if (guess_row < 0 or guess_row > (conf['rows'] - 1)) \
 		or (guess_col < 0 or guess_col > (conf['cols'] - 1)):
 			print(info['overflow'])
-		elif board[guess_row][guess_col] == 'X':
+		elif board[guess_row][guess_col] == conf['guessed']:
 			print(info['duplicate'])
 		else:
 			print(info['missed'])
-			board[guess_row][guess_col] = 'X'
+			board[guess_row][guess_col] = conf['guessed']
 
 		print_board()
