79e58ea82b90681cc3bafb581474c1ce803e2921
Kingsley <kingsley.xym@gmail.com>
2018-03-30 23:38:21 +0800

    :sparkles: Write Initial MultiThread Socket Server

--- Python/Socket/TCP_Server.py
+++ Python/Socket/TCP_Server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -6,17 +7,29 @@ ser = ('localhost', 2333)
 print('Starting socket server on {}:{}'.format(*ser))
 
 s.bind(ser)
-s.listen(1)
 
-while 1:
-	con, addr = s.accept()
-	try:
-		print('Connected with', addr)
-		while True:
-			data = con.recv(1024).decode()
-			if data == 'exit':
-				break
-			print('Received', data)
-			con.send(('I have received your data:' + data).encode())
-	finally:
-		con.close()
+def listener():
+	s.listen(1)
+
+	while 1:
+		con, addr = s.accept()
+		try:
+			print('Connected with', addr)
+			while True:
+				data = con.recv(1024).decode()
+				if data == 'exit':
+					break
+				print('Received', data)
+				con.send(('I have received your data:' + data).encode())
+		finally:
+			con.close()
+
+def sender():
+	# con.send(input().encode())
+	pass
+
+listen = threading.Thread(target = listener)
+send = threading.Thread(target = sender)
+
+listen.start()
+send.start()
