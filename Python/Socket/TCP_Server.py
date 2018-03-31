8a39f57251b07fe19a6bb4074e92369299994ab4
Kingsley <kingsley.xym@gmail.com>
2018-03-31 11:26:50 +0800

    :arrow_up: Support TCP Server With Multiple Client Connection

--- Python/Socket/TCP_Server.py
+++ Python/Socket/TCP_Server.py
@@ -1,18 +1,7 @@
 import socket
 import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-ser = ('localhost', 2333)
-print('Starting socket server on {}:{}'.format(*ser))
-
-s.bind(ser)
-
-def listener():
-	s.listen(1)
-
-	while 1:
-		con, addr = s.accept()
+def listener(con, addr):
 		try:
 			print('Connected with', addr)
 			while True:
@@ -28,8 +17,20 @@ def sender():
 	# con.send(input().encode())
 	pass
 
-listen = threading.Thread(target = listener)
-send = threading.Thread(target = sender)
 
-listen.start()
-send.start()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ser = ('localhost', 2333)
+print('Starting socket server on {}:{}'.format(*ser))
+
+s.bind(ser)
+s.listen(1)
+
+while 1:
+	con, addr = s.accept()
+	listen = threading.Thread(target = listener, args = (con, addr))
+	listen.start()
+
+# send = threading.Thread(target = sender)
+# send.start()
