7f4f47e1dcd4492fbd147a75ee84ed701e74779f
Kingsley <kingsley.xym@gmail.com>
2018-04-11 12:15:09 +0800

    :sparkles: Add More Implementations

--- Snippets/hash_demo.php
+++ Snippets/hash_demo.php
@@ -1,27 +1,31 @@
 <?php
 $password = '2333';
+$input = '2333';
 
+
+
+/*
+ * Implementation 1
+ */
 $salt = sha1(mt_rand());
-$salted_hash = hash('sha512', $password . $salt);
-
-echo json_encode([
-	'salt' => $salt,
-	'salted_hash' => $salted_hash
-]);
+$hash = password_hash($password, PASSWORD_DEFAULT, ['salt' => $salt]);
+$passed = password_verify($input, $hash);
 
 
 
-$right_input = '2333';
-$right_hash = hash('sha512', $right_input . $salt);
+/*
+ * Implementation 2 [PHP Version >= 7.0.0]
+ */
+$hash = password_hash($password, PASSWORD_DEFAULT);
+$passed = password_verify($input, $hash);
 
-echo json_encode([
-	'right_hash' => $right_hash,
-	'passed' => ($right_hash === $salted_hash)
-]);
 
-$wrong_input = '2332';
-$wrong_hash = hash('sha512', $wrong_input . $salt);
-echo json_encode([
-	'wrong_hash' => $wrong_hash,
-	'passed' => ($wrong_hash === $salted_hash)
-]);
+
+/*
+ * Implementation 3 [Not Recommended]
+ */
+$salt = sha1(mt_rand());
+$hash = hash('sha512', $password . $salt);
+
+$curr_hash = hash('sha512', $input . $salt);
+$passed = ($curr_hash === $hash);
