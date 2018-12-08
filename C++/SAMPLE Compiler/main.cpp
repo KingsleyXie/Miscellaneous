13725441503b54d44cfa1ac9ae69d07c7ceaa7ff
Kingsley <kingsley.xym@gmail.com>
2018-12-08 09:15:16 +0800

    :sparkles: Initialize Compiler Lab Framework

---
+++ C++/SAMPLE Compiler/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char const *argv[])
+{
+    std::string str = "HELLO\t";
+    str += "GDB TEST\n";
+
+    int num = 1;
+    num++;
+    num = 3;
+    num--;
+
+    return 0;
+}
