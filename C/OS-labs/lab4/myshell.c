f4430024effa13b8eaa16a8573030517e75518e1
Kingsley <kingsley.xym@gmail.com>
2018-12-17 03:47:08 +0800

    :tada: Finish Implementing Lab4

--- C/OS-labs/lab4/myshell.c
+++ C/OS-labs/lab4/myshell.c
@@ -9,6 +9,13 @@
 
 #define CH_MX 32
 
+// Polyfill for boolean value types
+typedef int bool;
+#define true 1
+#define false 0
+
+bool start_with(const char *pre, const char *str);
+
 int main(int argc, char* argv[])
 {
     printf("\n\033[1;33m----------------------- WELCOME -----------------------\n");
@@ -17,12 +24,17 @@ int main(int argc, char* argv[])
 
     char buffer_cmd[CH_MX];
     char *cmd = buffer_cmd;
+    char cwd[256];
 
     while (1)
     {
-        printf("\033[1;36m(myshell) $ \033[0m");
+        getcwd(cwd, sizeof(cwd));
+        printf("\033[1;36m(%s myshell) $ \033[0m", cwd);
+
         size_t ch_mx = CH_MX;
         getline(&cmd, &ch_mx, stdin);
+
+        size_t cmd_end = (strchr(cmd, '\n') - cmd);
 
         if (strcmp(cmd, "quit\n") == 0)
         {
@@ -30,10 +42,16 @@ int main(int argc, char* argv[])
             break;
         }
 
-        struct timeval start,end;
-        struct timezone tz;
-        gettimeofday(&start, &tz);
+        // Internal `exit` & `cd` command
+        if (start_with("exit", cmd)) exit(0);
+        if (start_with("cd", cmd))
+        {
+            char path[CH_MX] = {0};
+            strncpy(path, cmd + 3, cmd_end - 3);
+            chdir(path);
+        }
 
+        // Fork process for external jobs
         int pid = fork();
 
         if (pid < 0) printf("Error occured while forking\n");
@@ -43,7 +61,14 @@ int main(int argc, char* argv[])
         }
         else
         {
-            waitpid(pid, 0, 0);
+            // Background job ends with `&`
+            if (cmd[cmd_end - 1] != '&') waitpid(pid, 0, 0);
         }
     }
+}
+
+bool start_with(const char *pre, const char *str)
+{
+    size_t lenpre = strlen(pre), lenstr = strlen(str);
+    return lenstr < lenpre ? false : strncmp(pre, str, lenpre) == 0;
 }
