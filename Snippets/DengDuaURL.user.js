598719c4e872ff3d36dc20d21dab0027d19ee873
Kingsley <kingsley.xym@gmail.com>
2020-04-15 18:35:44 +0800

    :tada: Optimize UserScript

--- Snippets/DengDuaURL.user.js
+++ Snippets/DengDuaURL.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         DengDuaURL
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Make Court Wenshu Accessable Again!
 // @author       AJIE
-// @match        https://wenshu.court.gov.cn/*
+// @match        https://wenshu.court.gov.cn/content/content?DocID=*
 // @grant        none
 // ==/UserScript==
 
@@ -12,10 +12,11 @@
     'use strict';
     let url = window.location.href
     if (url.includes('DocID')) {
-        // https://wenshu.court.gov.cn/content/content?DocID=xxxxxxx
-        console.log('Old Format URL Detected, Redirecting...')
-        let docId = url.split('=')[1].replace(/-/g, '')
-        console.log('Doc ID:', docId)
-        window.location.href = 'https://wenshu.court.gov.cn/website/wenshu/181107ANFZ0BXSK4/index.html?docId=' + docId
+        let doc = `docId=${url.split('=')[1].replace(/-/g, '')}`
+        let ele = document.createElement('h2')
+        ele.innerHTML = `<center>${doc} - 即将重定向至新版文书阅读页面...</center>`
+        document.body.insertBefore(ele, document.querySelector('.main'))
+
+        window.location.href = `https://wenshu.court.gov.cn/website/wenshu/181107ANFZ0BXSK4/index.html?${doc}`
     }
 })();
